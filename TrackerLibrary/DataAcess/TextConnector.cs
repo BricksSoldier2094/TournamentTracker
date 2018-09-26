9f947115d11365ed26af8b470aef1dd423b29255
Henrique Souza <martins_henrique@uninove.edu.br>
2018-09-26 20:57:14 -0300

    Adicionado uma nova chave no app.config contendo o caminho onde nossa base de dados, baseada em arquivos, irá ser armazenada.
    Criado a classe TextConnectorProcessor para auxiliar a classe TextConnector, reunindo os métodos que serão utilizados na persistencia de qualquer Model na base de dados de arquivos.
    
    Criados os métodos extendidos:
    - FullFilePath
    Para obter o caminho onde a base de dados baseada em arquivos irá ser armazenada.
    
    - LoadFile
    Obtem o caminho completo do arquivo (fullfilepath) e carrega uma string correspondente ao nome do Model, para servir de nome do arquivo que irá conter os dados, que seram persistidos.
    
    - ConvertToPrizeModels
    Converte o arquivo de texto em uma lista de PrizeModels
    
    - SaveToPrizeFile
    Salva a lista de PrizeModels, já convertida em lista de string, no arquivo de texto.
    
    Agora o método CreatePrize da classe TextConnector fornece a funcionalidade de persistir dados na base de dados baseada em arquivos corretamente

--- TrackerLibrary/DataAcess/TextConnector.cs
+++ TrackerLibrary/DataAcess/TextConnector.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAcess.TextHelpers;
 
 namespace TrackerLibrary.DataAcess
 {
-    
+
 
     /// <summary>
     /// The TextConnector to access the TournamentTracker Text Database
     /// </summary>
     public class TextConnector : IDataConnection
     {
+        /// <summary>
+        /// The nome of the file that keeps the data of the prizes created
+        /// </summary>
+        private const string PrizesFile = "PrizeModels.csv";
 
         /// <summary>
         /// Create a new prize in the TournamentTracker Text DataBase
@@ -22,7 +27,26 @@ namespace TrackerLibrary.DataAcess
         /// <returns>The prize information, including the unique identifier</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            //Load the text file
+            //Convert the text file to a List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the max ID
+            int currentId = 1;
+
+            if(prizes.Count > 0)
+            {
+                currentId = currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+            }
+
+            model.Id = currentId;
+
+            // Add the new record with the new ID ( max ID + 1)
+            prizes.Add(model);
+
+            // Convert the Prizes to a list of a List<string>
+            // Save the List<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
---
+++ TrackerLibrary/DataAcess/TextConnectorProcessor.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackerLibrary;
+using TrackerLibrary.Models;
+
+
+
+namespace TrackerLibrary.DataAcess.TextHelpers
+
+{
+    public static class TextConnectorProcessor
+    {
+        /// <summary>
+        /// Return the path of the file based database
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static string FullFilePath(this string fileName) //PrizeModels.csv
+        {
+            // string C:\Data\TournamentTracker\PrizeModels.csv
+            return $"{ConfigurationManager.AppSettings["filePath"] } \\ { fileName } ";
+        }
+
+        /// <summary>
+        /// Takes the full file path and load the string
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        public static List<string> LoadFile(this string file)
+        {
+            //validates if the file already exists
+            if (!File.Exists(file))
+            {
+                return new List<string>();
+            }
+
+            //read all the lines in the file and create a list of strings with it
+            return File.ReadAllLines(file).ToList();
+
+        }
+
+       /// <summary>
+       /// Convert the text to a list of PrizeModels
+       /// </summary>
+       /// <param name="lines"></param>
+       /// <returns></returns>
+        public static List<PrizeModel> ConvertToPrizeModels(this List<string> lines)
+        {
+            List<PrizeModel> output = new List<PrizeModel>();
+
+            foreach (string line in lines)
+            {
+                string[] cols = line.Split(',');
+
+                PrizeModel p = new PrizeModel();
+                p.Id = int.Parse(cols[0]);
+                p.PlaceNumber = int.Parse(cols[1]);
+                p.PlaceName = cols[2];
+                p.PrizeAmount = decimal.Parse(cols[3]);
+                p.PrizePercentage = double.Parse(cols[4]);
+                output.Add(p);                
+            }
+            return output;
+        }
+
+        /// <summary>
+        /// Saves the list of models, like a list of strings, in the file.
+        /// </summary>
+        /// <param name="models"></param>
+        /// <param name="fileName"></param>
+        public static void SaveToPrizeFile(this List<PrizeModel> models, string fileName)
+        {
+            List<string> lines = new List<string>();
+
+            foreach (PrizeModel p in models)
+            {
+                lines.Add($"{ p.Id },{ p.PlaceNumber },{ p.PlaceName },{ p.PrizeAmount },{ p.PrizePercentage }");
+            }
+
+            File.WriteAllLines(fileName.FullFilePath(), lines);
+
+        }
+
+    }
+}
